c6c8abb0a0bd0c37c73726c3cd8fbf308867efd8
lerakorzhova <lerakorzhova98@gmail.com>
2017-02-17 20:15:31 +0300

    min-base: test version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function getBase(base) {
-  // your solution
+module.exports = function getBase(num) {
+    var copy = num,
+        base = '',
+        length = (num + '').length - 1;
+
+    if (num % Math.pow(10, length) === 0) {
+        for (var i = 0; i < length; i++) {
+            base = base + '9';
+        }
+        return base;
+    }
+
+    base = 2;
+    while (copy) {
+        if (copy % base === 1) {
+            copy = Math.floor(copy / base);
+        } else {
+            copy = num;
+            base++;
+        }
+
+    }
+    return base;
 }
